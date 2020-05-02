a79315a6bb540ed26a13d9082db0ad6a27faa2d0
mustafatamyapar <mustafatamyapar@users.noreply.github.com>
2020-05-02 22:25:23 +0300

    by Mustafa Efe Tamyapar. Working on diseases page

--- app/src/main/java/com/example/aquaassistant/mustafa/DiseasesPage.java
+++ app/src/main/java/com/example/aquaassistant/mustafa/DiseasesPage.java
@@ -1,14 +1,93 @@
 package com.example.aquaassistant.mustafa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.aquaassistant.MainActivity;
+import com.example.aquaassistant.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiseasesPage extends AppCompatActivity {
+
+    private final String JSON_URL = "https://gist.githubusercontent.com/mustafatamyapar/e9f87d0be83918469c7ed111bcb766bd/raw/0a1d404a036a7a88dc34d3e1ba30e4d67ad3f6a7/gistfile1.txt";
+    private JsonArrayRequest request;
+    private RequestQueue requestQueue;
+    private List<Disease> listDisease;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_diseases_page);
+        setContentView(R.layout.activity_diseases_page);
+
+        listDisease = new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerwiewid);
+        jsonrequest();
+
+
+    }
+    private void jsonrequest() {
+        request = new JsonArrayRequest(JSON_URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                JSONObject jsonObject = null;
+
+                for ( int i = 0; i < response.length(); i++)
+                {
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        Disease disease = new Disease();
+                        disease.setName(jsonObject.getString("name"));
+                        disease.setDescription(jsonObject.getString("description"));
+                        disease.setSymptoms(jsonObject.getString("symptoms"));
+                        disease.setImageUrl(jsonObject.getString("img"));
+                        listDisease.add(disease);
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                setUpRecyclerView(listDisease);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        requestQueue = Volley.newRequestQueue(DiseasesPage.this);
+        requestQueue.add(request);
+
+
+    }
+
+    private void setUpRecyclerView(List<Disease> listDisease) {
+
+        RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this,listDisease);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(myAdapter);
+
     }
 }
--- app/src/main/java/com/example/aquaassistant/mustafa/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/aquaassistant/mustafa/RecyclerViewAdapter.java
@@ -5,6 +5,11 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.aquaassistant.mustafa.Disease;
 import com.example.aquaassistant.R;
 import androidx.annotation.NonNull;
@@ -16,10 +21,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     private Context mContext;
     private List<Disease> mData;
+    RequestOptions option;
 
     public RecyclerViewAdapter(Context mContext, List<Disease> mData) {
         this.mContext = mContext;
         this.mData = mData;
+
+        option = new RequestOptions().centerCrop().placeholder(R.drawable.shape_diseases);
     }
 
     @NonNull
@@ -28,13 +36,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         View view;
         LayoutInflater inflater = LayoutInflater.from(mContext);
-      //  view = inflater.inflate(R.layout.disease_row_item)
-        return null;
+        view = inflater.inflate(R.layout.disease_row_item,parent,false);
+
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
+        holder.diseaseName.setText(mData.get(position).getName());
+        holder.symptoms.setText(mData.get(position).getSymptoms());
+
+        Glide.with(mContext).load(mData.get(position).getImageUrl()).apply(option).into(holder.img_thumbnail);
     }
 
     @Override
@@ -43,8 +56,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
-        public MyViewHolder(View view){
+        TextView diseaseName;
+        TextView symptoms;
+        ImageView img_thumbnail;
+
+
+        public MyViewHolder(View itemView){
+
             super(itemView);
+
+            diseaseName = itemView.findViewById(R.id.disease_name);
+            symptoms = itemView.findViewById(R.id.Symptoms);
+            img_thumbnail = itemView.findViewById(R.id.thumbnail);
         }
     }
 }
