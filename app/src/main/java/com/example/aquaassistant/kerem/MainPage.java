54b8f0f8cb072d66d137de5e7bf19f531ac95289
Kerem <63609393+Kerem-Tayhan@users.noreply.github.com>
2020-05-07 11:40:44 +0300

    tidying up the main page and also incorporating a sha1 encrypter

---
+++ app/src/main/java/com/example/aquaassistant/kerem/MainPage.java
@@ -0,0 +1,22 @@
+package com.example.aquaassistant.kerem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.aquaassistant.R;
+import com.example.aquaassistant.sena.SettingsMain;
+import com.example.aquaassistant.zulal.Faqactivity;
+
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
