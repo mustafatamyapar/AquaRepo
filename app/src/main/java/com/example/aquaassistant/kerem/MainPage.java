016bbdae2e18678d3bcc326b356fbf60a73a9fd2
Kerem <63609393+Kerem-Tayhan@users.noreply.github.com>
2020-05-08 22:06:07 +0300

    solved the main page issue

--- app/src/main/java/com/example/aquaassistant/kerem/MainPage.java
+++ app/src/main/java/com/example/aquaassistant/kerem/MainPage.java
@@ -8,15 +8,46 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.aquaassistant.R;
+import com.example.aquaassistant.mustafa.EncyclopediaPage;
 import com.example.aquaassistant.sena.SettingsMain;
+import com.example.aquaassistant.zeynep.TankPageActivity;
 import com.example.aquaassistant.zulal.Faqactivity;
 
 
 public class MainPage extends AppCompatActivity {
 
+    private Button tanksButton;
+    private Button profileButton;
+    private Button settingButton;
+    private Button encyclopediaButton;
+    private Button remindersButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        tanksButton= findViewById(R.id.tankButton);
+        profileButton = findViewById(R.id.profileButton);
+        settingButton = findViewById(R.id.settingsButton);
+        encyclopediaButton = findViewById(R.id.encyclopedia);
+        remindersButton = findViewById(R.id.button7);
+
+    }
+    public void openSettings(View view){
+        Intent intent = new Intent( this, SettingsMain.class);
+        startActivity(intent);
+    }
+    public void openTanks(View view){
+        Intent intent = new Intent( this, TankPageActivity.class);
+        startActivity(intent);
+    }
+    public void openEncyclopedia(View view){
+        Intent intent = new Intent( this, EncyclopediaPage.class);
+        startActivity(intent);
+    }
+    public void openProfile(View view){
+        Intent intent = new Intent( this, ProfilePage.class);
+        startActivity(intent);
     }
 }
