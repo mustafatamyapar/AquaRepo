7aa6c3e08ed468a4eb1924ddffa1af1d15af597e
ZeynepBerber <63536764+ZeynepBerber@users.noreply.github.com>
2020-05-09 05:14:06 +0300

    by Zeynep

--- app/src/main/java/com/example/aquaassistant/zeynep/FavouritePlacesActivity.java
+++ app/src/main/java/com/example/aquaassistant/zeynep/FavouritePlacesActivity.java
@@ -3,14 +3,21 @@ package com.example.aquaassistant.zeynep;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.aquaassistant.R;
 
 public class FavouritePlacesActivity extends AppCompatActivity {
+    ListView favouritesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite_places);
+    }
+    public void addNewPlace(View view){
+
     }
 }
