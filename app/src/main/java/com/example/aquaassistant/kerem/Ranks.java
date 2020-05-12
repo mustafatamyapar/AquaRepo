0119561477f44df2d82c5290d14534073e5cbc25
Kerem-Tayhan <63609393+Kerem-Tayhan@users.noreply.github.com>
2020-05-12 09:02:46 +0300

    the most anticipated experience bar and ranks...

---
+++ app/src/main/java/com/example/aquaassistant/kerem/Ranks.java
@@ -0,0 +1,17 @@
+package com.example.aquaassistant.kerem;
+
+public class Ranks {
+
+    private String BEGINNER = "Begginer";
+    private String INTERMEDIATE = "Intermediate";
+    private String ADVANCED = "Advanced";
+    private String EXPERT = "Expert";
+
+    public Ranks(){
+
+    }
+    public void changeRank(){
+
+    }
+
+}
