6e3b799e968209b2c37130a5b3842661b56f4041
Kaan Özkan <kaan.ozkan@ug.bilkent.edu.tr>
2020-05-19 18:15:00 +0300

    by Kaan Ozkan

---
+++ app/src/main/java/com/example/aquaassistant/kaan/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.example.aquaassistant.kaan;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.aquaassistant.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
